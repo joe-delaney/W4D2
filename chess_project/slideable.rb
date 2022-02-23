970f9bfb2798c57aa7797455dd83a4e0290b1133
joe-delaney <joseph.delaney546@gmail.com>
2022-02-22 21:15:14 -0500

    commented slideable

--- chess_project/slideable.rb
+++ chess_project/slideable.rb
@@ -1,4 +1,5 @@
 module Slideable 
+  #Represents one step in each direction horizontally and diagonally
   HORIZONTAL_DIRS = [[0,1], [1,0], [0,-1],[-1,0]]
   DIAGONAL_DIRS = [[1,1],[1,-1],[-1,1],[-1,-1]] 
 
@@ -10,9 +11,15 @@ module Slideable
     DIAGONAL_DIRS
   end
 
+  #This will return the possible moves for the piece that called it
   def moves
     possible_moves = []
+    #Call move_dirs to get the possible directions this piece can move in
+    #The kind of piece calling moves will determine the valid directions
     directions = move_dirs
+
+    #Go through each direction and pass it to helper method
+    #helper method will return an array of possible moves in that direction
     directions.each do |direction|
       dx = direction[0]
       dy = direction[1]
@@ -27,7 +34,10 @@ module Slideable
     horizontal_dirs + diagonal_dirs
   end
 
-  require "byebug"
+  #This will return the possible moves a piece can make in one direction
+  #Slideable pieces are able to move as far as they want in one direction
+  #as long as they remain in the boundaries of the board (0..7) and are not
+  #blocked by another piece of its color
   def grow_unblocked_moves_in_dr(dx, dy)
     start_pos = pos 
     unblocked_moves = []
@@ -37,12 +47,10 @@ module Slideable
     while (new_row >= 0 && new_col >= 0 && new_row < board.length && 
       new_col < board.length && (board[new_pos].is_a?(NullPiece) || 
       board[new_pos].color != color))
-
       unblocked_moves << new_pos
       new_row = new_row + dx
       new_col = new_col + dy
       new_pos = [new_row, new_col]
-      
     end
     unblocked_moves
   end
