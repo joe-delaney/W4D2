5ec409514f2429675dad3bee3aecf32093eeeede
App Academy Student <email@email.com>
2022-02-23 09:26:19 -0500

    finished side attacks

--- chess_project/pawn.rb
+++ chess_project/pawn.rb
@@ -9,7 +9,6 @@ class Pawn < Piece
     @symbol
   end
 
-  require "byebug"
   def moves
     possible_moves = []
 
@@ -21,13 +20,25 @@ class Pawn < Piece
         possible_moves << new_pos 
       end
     end
+
+    possible_attacks = side_attacks(pos)
+    possible_attacks.each do |attack_pos|
+      possible_moves << attack_pos if valid_attack?(attack_pos)
+    end
     possible_moves
   end
 
   private 
   def valid?(pos)
     row, col = pos 
-    row >= 0 && col >= 0 && row < board.length && col < board.length && board[pos].is_a?(NullPiece)
+    (row >= 0 && col >= 0 && row < board.length && col < board.length 
+    && board[pos].is_a?(NullPiece))
+  end
+
+  def valid_attack?(pos)
+    row, col = pos 
+    (row >= 0 && col >= 0 && row < board.length && col < board.length 
+    && board[pos].color != color && !board[pos].is_a?(NullPiece))
   end
 
   def forward_dir
@@ -46,6 +57,18 @@ class Pawn < Piece
     return true if color == :white && pos[0] == 6
     return true if color == :black && pos[0] == 1
     return false
+  end
+
+  def side_attacks(pos)
+    row, col = pos
+    if color == :white
+      possible_attack_1 = [row-1][col-1]
+      possible_attack_2 = [row-1][col+1]
+    else
+      possible_attack_1 = [row+1][col-1]
+      possible_attack_2 = [row+1][col+1]
+    end
+    [possible_attack_1, possible_attack_2]
   end
 
 end
